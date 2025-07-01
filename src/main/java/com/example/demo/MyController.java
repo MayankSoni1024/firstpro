9bd9805fdedeffedd285b062c3dab2cf29fc7541
MayankSoni1024 <mayank.soni1024@gmail.com>
2025-07-01 14:05:23 +0530

    first commit

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
 public class MyController {
 	@RequestMapping("/first")
 	public String first() {
