52291d6005e1d8fa40002225a42a8114412baa88
MayankSoni1024 <mayank.soni1024@gmail.com>
2025-07-01 13:52:01 +0530

    first commit

---
+++ src/main/java/com/example/demo/FirstWebProApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebProApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/first")
+	public String first() {
+		System.out.println("first is called");
+		return "first";
+	}
+	@RequestMapping("/second")
+	public String second() {
+		System.out.println("second is called");
+		return "first";
+	}
+}
